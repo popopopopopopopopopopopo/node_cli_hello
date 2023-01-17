962e2741e2721d7fa86f9055a7e4f3f5521ab4c0
kosun <temparamental@hotmail.com>
2023-01-17 13:52:40 +0900

    コマンドライン引数をオプション化&markdownをhtml出力する機能の追加

---
+++ README.md
@@ -0,0 +1,4 @@
+## 現時点での実行
+```shell
+node main.js --f ./sample.md --gfm sample.md 
+```
--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 import { program } from "commander";
 import * as fs from "node:fs/promises";
+import { marked } from "marked";
 
 // fileオプションを定義する
-program.option("--f", "対象ファイル");
 // gfmオプションを定義する
-program.option("--gfm", "GFMを有効にする");
+program.option("-f, --file <n>", "対象ファイル", "")
+  .option("-gfm, --gfm <n>", "GFMを有効にする", false)
+  .parse(process.argv);
 
 const options = program.opts();
-program.parse(process.argv);
 
 // コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
 const cliOptions = {
-  filePath: options.f ?? "",
+  filePath: options.file ?? "",
   gfm: options.gfm ?? false,
 };
 
-const file = await fs.readFile(cliOptions.filePath).catch(e => {
+const file = await fs.readFile(cliOptions.filePath, 'utf8').catch(e => {
   console.error(e);
   process.exit(1);
 });
+
+const html = marked.parse(file);
+console.log(html);
